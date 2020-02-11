8b3d1b72c5b03d66d4acd68158aaa4817f9411cc
assadakramiintellect <assad@iintellect.co.uk>
2020-02-11 17:28:50 +0500

    complete

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+import React from "react";
+interface propsObject {
+    data: {}[];
+    Component: React.StatelessComponent<any>;
+    wrapperClass?: string;
+    scrollLength?: number;
+    showControls?: boolean;
+}
+declare const HorizontalScroll: React.FC<propsObject>;
+export default HorizontalScroll;
+//# sourceMappingURL=index.d.ts.map
